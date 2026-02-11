5b38a4fb29e88b624c212067ec2da5283c90b9a0
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2026-02-11 13:10:53 -0600

    Access current weather data from fetch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import "./styles.css";
 
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeather(form.location.value);
+  form.reset();
+});
+
 async function getWeather(location = "London") {
   const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=24N7WG5T3Y8VAWC8BV7HSMSPG`,
   );
-  const data = response.json();
+  const data = await response.json();
+  const currentWeather = data.currentConditions.temp;
   console.log(data);
+  console.log(currentWeather);
 }
 
 getWeather();
