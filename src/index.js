381aa516cf9f03d62643a4e352008703ef910eb8
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2026-02-11 12:11:41 -0600

    Set up basic javascript code to fetch weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+async function getWeather(location = "London") {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=24N7WG5T3Y8VAWC8BV7HSMSPG`,
+  );
+  const data = response.json();
+  console.log(data);
+}
+
+getWeather();
