27d3659a310db9e6a17e871b757807ecebadcc7f
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2026-02-12 15:37:59 -0600

    Add catch error to weather function

--- src/index.js
+++ src/index.js
@@ -11,17 +11,20 @@ form.addEventListener("submit", (e) => {
 });
 
 async function getWeather(location = "London") {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=24N7WG5T3Y8VAWC8BV7HSMSPG`,
-  );
-  const data = await response.json();
-  const currentLocation = data.address;
-  const currentWeather = data.currentConditions.temp;
-  const weatherIcon = data.currentConditions.icon;
-  img.src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${weatherIcon}.svg`;
-  temperatureDisplay.innerText = `${currentLocation}: ${currentWeather}°F`;
-  console.log(currentWeather);
-  console.log(data);
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=24N7WG5T3Y8VAWC8BV7HSMSPG`,
+    );
+    const data = await response.json();
+    const currentLocation = data.address;
+    const currentWeather = data.currentConditions.temp;
+    const weatherIcon = data.currentConditions.icon;
+    img.src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${weatherIcon}.svg`;
+    temperatureDisplay.innerText = `${currentLocation}: ${currentWeather}°F`;
+  } catch (error) {
+    console.error("Error fetching weather:", error);
+    temperatureDisplay.innerText = "Unable to fetch weather data";
+  }
 }
 
 getWeather();
